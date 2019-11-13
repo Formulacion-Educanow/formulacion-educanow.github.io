9d692e0cda9178e59ea7599f79d01af944332e2d
Andres Lopez <daztery@gmail.com>
2019-11-12 19:39:02 -0500

    Finished

--- js/form-email.js
+++ js/form-email.js
@@ -1,13 +1,21 @@
 const btnSend = document.getElementById("form-send");
 
 btnSend.onclick = e => {
-    const email = document.getElementById("form-email").value;
-    const name = document.getElementById("form-name").value;
-    const message = document.getElementById("form-message").value;
-    const body = { name, email, message };
+    const email = document.getElementById("form-email");
+    const name = document.getElementById("form-name");
+    const message = document.getElementById("form-message");
+    const body = { 
+        name: name.value, 
+        email: email.value, 
+        message: message.value 
+    };
     console.log(JSON.stringify(body));
+    const headers = new Headers({
+        "Content-Type": "application/json"
+    });
     fetch('https://formu-educanow.herokuapp.com/send',{
         method: 'POST',
+        headers,
         body: JSON.stringify(body)
     }).then(response => {
         if(response.ok){
@@ -16,6 +24,13 @@ btnSend.onclick = e => {
         throw new Error('Request Failed!');
     },networkError => console.log(networkError.message))
     .then(jsonResponse => {
+        name.value = "";
+        email.value = "";
+        message.value = "";
+        Swal.fire(
+            'Correo enviado',
+            'Tu mensaje ha sido enviado satisfactoriamente',
+            'success')
         console.log(jsonResponse);
     });
 };
